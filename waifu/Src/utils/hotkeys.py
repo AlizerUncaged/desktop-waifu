494756815feb5758c0430293649004f5ea750d07
matrixfox <matrixfox@gmail.com>
2024-02-26 20:57:11 -0800

    Fixed PTT Not Responding
    This way uses less CPU. Time is useless!

--- waifu/Src/utils/hotkeys.py
+++ waifu/Src/utils/hotkeys.py
@@ -1,5 +1,4 @@
 import keyboard
-import time
 
 KEYS = ["RIGHT_CTRL"]
 
@@ -7,5 +6,4 @@ def audio_input():
     return all(keyboard.is_pressed(key) for key in KEYS)
 
 def audio_input_await():
-    while not audio_input():
-        time.sleep(0.1)
+    keyboard.wait(KEYS[0])
